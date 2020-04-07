4c4ff9c77c0efe3f9c8ba73dc152f481547079ce
Nick Stewart <nick@viastudio.com>
2020-04-07 15:10:43 -0400

    Setup basic styling

--- frontend/components/layout.tsx
+++ frontend/components/layout.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { Global, css } from '@emotion/core'
 
 type LayoutProps = {
     readonly children: React.ReactNode;
@@ -9,7 +10,25 @@ const Layout = ({ children }: LayoutProps) => (
         <Head>
             <title>Create Next App</title>
             <link rel='icon' href='/favicon.ico' />
+            <link href='https://fonts.googleapis.com/css2?family=Roboto' rel='stylesheet' />
         </Head>
+        <Global
+            styles={css`
+                html {
+                    box-sizing: border-box;
+                }
+                *,
+                *:before,
+                *:after {
+                    box-sizing: inherit;
+                }
+                body {
+                    margin: 0;
+                    background: #eef0f7;
+                    font-family: 'Roboto', sans-serif;
+                }
+            `}
+        />
         <main>{children}</main>
     </>
 );
